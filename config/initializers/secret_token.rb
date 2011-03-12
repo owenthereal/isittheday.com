b247e8dcfa11f15c401e9f602a0b1e6598882776
Jingwen Owen Ou <jingweno@gmail.com>
2011-03-11 16:18:56 -0800

    Project init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IsitthedayCom::Application.config.secret_token = 'f2a1ce1b29e5bba16be88e20c04f9373e1471e970807af677068e5e2613a0f7a03ff170a17ed1144c390fd048d257f0292d797d7491e0e3a43fbfbf62f28803c'
